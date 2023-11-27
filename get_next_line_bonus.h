d8384d37454f2c1de7461edf3844d8add697d6d0
Yim Jiun Jye <105974423+SkyHearts@users.noreply.github.com>
2023-11-27 17:40:40 +0800

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 11:34:08 by jyim              #+#    #+#             */
+/*   Updated: 2022/11/25 13:53:17 by jyim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 42
+// #endif
+
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src);
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 10:07:49 by jyim              #+#    #+#             */
+/*   Updated: 2023/06/22 16:47:06 by jyim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	c;
+
+	c = 0;
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void			*ptr;
+	size_t			n;
+	unsigned char	*temp;
+
+	n = count * size;
+	ptr = malloc(n);
+	if (!ptr)
+		return (0);
+	temp = (unsigned char *)ptr;
+	while ((n) > 0)
+	{
+	*temp++ = 0;
+	n--;
+	}
+	return (ptr);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	join_len;
+	size_t	i;
+	size_t	j;
+	char	*strjoin;
+
+	if (s1 == NULL && s2 == NULL)
+		return (ft_strdup(""));
+	join_len = ft_strlen(s1) + ft_strlen(s2);
+	strjoin = malloc(sizeof(char) * (join_len) + 1);
+	if (!strjoin)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		strjoin[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+		strjoin[i++] = s2[j++];
+	strjoin[i] = '\0';
+	return (strjoin);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*temp_s;
+
+	temp_s = (char *)s;
+	while (*temp_s != (char)c)
+	{
+		if (!*temp_s)
+			return (NULL);
+		temp_s++;
+	}
+	return (temp_s);
+}
+
+char	*ft_strdup(const char *src)
+{
+	int		size;
+	int		c;
+	char	*srccpy;
+
+	size = 0;
+	c = 0;
+	while (src[size] != '\0')
+		size++;
+	srccpy = (char *) malloc(sizeof(char) * size + 1);
+	if (srccpy)
+	{
+		while (src[c] != '\0')
+		{
+			srccpy[c] = src[c];
+			c++;
+		}
+		srccpy[c] = '\0';
+		return (srccpy);
+	}
+	else
+		return (NULL);
+}
